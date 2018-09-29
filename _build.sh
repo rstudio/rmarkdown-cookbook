6d7b8db509d824cf5f67fe659d4f5e370af11a59
Michael Harper <mikey-harper@hotmail.com>
2018-09-29 20:12:23 +0100

    Turn off TravisCI PDF build

--- _build.sh
+++ _build.sh
@@ -2,7 +2,7 @@
 webshot::install_phantomjs()
 
 bookdown::render_book('index.Rmd', 'bookdown::gitbook')
-bookdown::render_book("index.Rmd", "bookdown::pdf_book")
-bookdown::render_book("index.Rmd", "bookdown::epub_book")
+# bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+# bookdown::render_book("index.Rmd", "bookdown::epub_book")
 
 
