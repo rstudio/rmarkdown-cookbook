681178f223ed6a5e60f94e16f10feb2ce642e24b
Christophe Dervieux <christophe.dervieux@gmail.com>
2019-08-12 22:04:36 +0200

    Add a part about cat engine (#167)
    *  :page_facing_up: Add recipe about cat engine
    
    * add examples
    
    * rename example
    
    * format line width
    
    * Load Rmd example for the book from the example folder
    
    * new dependencies
    
    * typo

---
+++ book/build_book_utils.R
@@ -0,0 +1,10 @@
+import_example <- function(file) {
+  example <- xfun::read_utf8(here::here("examples", file))
+  # example <- gsub("^(```\\{)", "`r ''`\\1", example)
+  example <- c(
+    "````md",
+    example,
+    "````"
+  )
+  cat(example, sep = "\n")
+}
