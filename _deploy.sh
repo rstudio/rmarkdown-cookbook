ffff8cb91232355557fa58656a863032d45c71e8
Yihui Xie <xie@yihui.name>
2019-08-12 22:40:57 -0500

    make sure all old pages are deleted before copying and deploying new pages

--- _deploy.sh
+++ _deploy.sh
@@ -10,6 +10,11 @@ git config --global user.name "Michael Harper"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
+
+rm -r .Rproj.user
+ls | xargs rm -rf
+git ls-files --deleted -z | xargs -0 git rm
+
 cp -r ../book/_book/* ./
 git add --all *
 git commit -m"Update the book" || true
