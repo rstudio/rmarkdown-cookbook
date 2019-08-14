c1eec26a8b204270b3d588ce406c86b6089ef0da
Yihui Xie <xie@yihui.name>
2019-08-14 11:23:37 -0500

    move Rmd source files to the root level

--- build_book_utils.R
+++ build_book_utils.R
@@ -1,5 +1,5 @@
 import_example <- function(file) {
-  example <- xfun::read_utf8(file.path("../examples", file))
+  example <- xfun::read_utf8(file.path("examples", file))
   # example <- gsub("^(```\\{)", "`r ''`\\1", example)
   example <- c(
     "````md",
