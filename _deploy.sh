91d52e9a835c1008190805679f91d7262fb44954
Yihui Xie <xie@yihui.name>
2019-11-22 16:25:41 -0600

    do not keep the git history in the gh-pages branch

--- _deploy.sh
+++ _deploy.sh
@@ -5,8 +5,8 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "m.harper@outlook.com"
-git config --global user.name "Michael Harper"
+git config --global user.email "xie@yihui.name"
+git config --global user.name "Yihui Xie"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
@@ -17,4 +17,5 @@ git ls-files --deleted -z | xargs -0 git rm
 cp -r ../_book/* ./
 git add --all *
 git commit -m"Update the book" || true
-git push -q origin gh-pages
+git reset $(git commit-tree HEAD^{tree} -m "Update the book")
+git push -f -q origin gh-pages
