427bddcfba27fb2b54d2ccf06dcd99cacf571458
Christophe Dervieux <christophe.dervieux@gmail.com>
2021-05-25 15:31:12 +0200

    don't deploy on CI for now

--- _render.R
+++ _render.R
@@ -1,6 +1,6 @@
 quiet = "--quiet" %in% commandArgs(FALSE)
 formats = commandArgs(TRUE)
-travis = !is.na(Sys.getenv('CI', NA))
+CI = !is.na(Sys.getenv('CI', NA))
 
 # provide default formats if necessary
 if (length(formats) == 0) formats = c('bookdown::pdf_book', 'bookdown::gitbook')
@@ -12,15 +12,7 @@ for (fmt in formats) {
 }
 unlink('rmarkdown-cookbook.log')
 
-r = '<body onload="window.location = \'https://bookdown.org/yihui\'+location.pathname">'
-if (travis) for (f in list.files('_book', '[.]html$', full.names = TRUE)) {
-  x = readLines(f)
-  if (length(i <- grep('^\\s*<body>\\s*$', x)) == 0) next
-  # patch HTML files in gh-pages if built on Travis, to redirect to bookdown.org
-  x[i[1]] = r
-  writeLines(x, f)
-}
-
+# creates redirects
 redirect = function(from, to) {
   to = paste0('https://bookdown.org/yihui/rmarkdown-cookbook/', to)
   writeLines(sprintf(
@@ -31,6 +23,6 @@ redirect = function(from, to) {
 redirect('r-markdown-components.html', 'rmarkdown-process.html')
 redirect('acknowledgements.html', 'acknowledgments.html')
 
-if (length(formats) > 1 && Sys.getenv('USER') == 'yihui') {
+if (!CI && length(formats) > 1 && Sys.getenv('USER') == 'yihui') {
   bookdown::publish_book(account = 'yihui', server = 'bookdown.org')
 }
